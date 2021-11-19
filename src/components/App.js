7aed5376f5cee19241f32b5751728b442d82fcc9
Connor Hansen <chansen13@georgefox.edu>
2021-11-19 13:26:09 -0800

    add header section, begin styling.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import '../App.css';
 import KegControl from "./KegControl";
-
+import Header from "./Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <React.Fragment>
-      {/* <Header /> */}
-      <KegControl />
+      <body style={{backgroundColor:'gray'}}>
+      <Container>
+        <Row>
+          <Header />
+        </Row>
+        <Row>
+          <KegControl />
+        </Row>
+      </Container>
+      </body>
     </React.Fragment>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header(){
+  return (
+    <React.Fragment>
+      <h2>Welcome to the Tap Room!</h2>
+    </React.Fragment>
+  );
+}
+
+export default Header;
